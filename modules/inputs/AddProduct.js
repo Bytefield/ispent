a1325611c602d5bcf02fa1de3cd830c3fe030e34
Bytefield <ramosperez1978@gmail.com>
2023-03-14 08:55:59 +0100

    added wireframe to code

--- modules/inputs/AddProduct.js
+++ modules/inputs/AddProduct.js
@@ -2,24 +2,24 @@ import React, { useState } from 'react';
 import { View, TextInput, Button } from 'react-native';
 
 export default function AddProduct({ addProduct }) {
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState('');
-  const [quantity, setQuantity] = useState('');
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [quantity, setQuantity] = useState('');
 
-  const handleSubmit = () => {
-    const newProduct = { name, price: Number(price), quantity: Number(quantity) };
-    addProduct(newProduct);
-    setName('');
-    setPrice('');
-    setQuantity('');
-  };
+    const handleSubmit = () => {
+        const newProduct = { name, price: Number(price), quantity: Number(quantity) };
+        addProduct(newProduct);
+        setName('');
+        setPrice('');
+        setQuantity('');
+    };
 
-  return (
-    <View>
-      <TextInput placeholder="Name" value={name} onChangeText={setName} />
-      <TextInput placeholder="Price" value={price} onChangeText={setPrice} />
-      <TextInput placeholder="Quantity" value={quantity} onChangeText={setQuantity} />
-      <Button title="Add Product" onPress={handleSubmit} />
-    </View>
-  );
+    return (
+        <View>
+            <TextInput placeholder="Name" value={name} onChangeText={setName} />
+            <TextInput placeholder="Price" value={price} onChangeText={setPrice} />
+            <TextInput placeholder="Quantity" value={quantity} onChangeText={setQuantity} />
+            <Button title="Add Product" onPress={handleSubmit} />
+        </View>
+    );
 }
--- modules/inputs/Barcode.js
+++ modules/inputs/Barcode.js
@@ -3,36 +3,51 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function BarCode({ onBarCodeScanned, onCancel }) {
-  const [hasPermission, setHasPermission] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanning, setScanning] = useState(true);
+    const [scannedData, setScannedData] = useState(false);
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    onBarCodeScanned(data);
-  };
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
 
-  const handleCancel = () => {
-    onCancel();
-  }
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanning(false);
+        setScannedData(data !== null);
+        onBarCodeScanned(data);
+    };
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-  return (
-    <View style={{ flex: 1 }}>
-      <BarCodeScanner
-        onBarCodeScanned={handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      <Button title="Cancel" onPress={handleCancel} />
-    </View>
-  );
+    const handleCancel = () => {
+        onCancel();
+    }
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+    return (
+        <View style={{ flex: 1 }}>
+            { scanning ? (
+                <BarCodeScanner
+                    onBarCodeScanned={handleBarCodeScanned}
+                    style={StyleSheet.absoluteFillObject}
+                />
+            ) : (
+                <View>
+                    {scannedData.success ? (
+                        <ProductDetails data={scannedData.data} />
+                    ) : (
+                        <AddNewProduct data={scannedData.data} />
+                    )}
+                </View>
+            )}
+            <Button title="Cancel" onPress={handleCancel} />
+        </View>
+    );
 }
